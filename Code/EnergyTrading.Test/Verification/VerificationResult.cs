0309f360d04d388757d53dfc44ba0a718590d104
Simon Hanlon <simon.hanlon@rwe.com>
2014-04-15 16:50:05 +0100

    Added VerificationResult

---
+++ Code/EnergyTrading.Test/Verification/VerificationResult.cs
@@ -0,0 +1,13 @@
+﻿namespace EnergyTrading.Test.Verification
+{
+    using System;
+
+    public class VerificationResult
+    {
+        public string Code { get; set; }
+
+        public Guid TestId { get; set; }
+
+        public string MessageResult { get; set; }
+    }
+}
