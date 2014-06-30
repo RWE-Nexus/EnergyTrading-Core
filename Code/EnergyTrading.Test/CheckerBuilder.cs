bf96ca3b188b33a8d22f717083b18ffda95ff861
Paul Hatcher <paul.hatcher@rwe.com>
2014-06-30 08:46:36 +0100

    Restore local CheckerBuilder with obsolete warning to avoid project failures

---
+++ Code/EnergyTrading.Test/CheckerBuilder.cs
@@ -0,0 +1,19 @@
+﻿namespace EnergyTrading.Test
+{
+    using System;
+
+    /// <summary>
+    /// Use reflection to construct a checker for a type
+    /// </summary>
+    [Obsolete("Use NCheck.CheckerBuilder, also CheckerBuilder is now assigned in default CheckerFactory")]
+    public class CheckerBuilder : NCheck.CheckerBuilder
+    {
+        /// <summary>
+        /// Create a new instance of the <see cref="CheckerBuilder" /> class.
+        /// </summary>
+        /// <param name="factory"></param>
+        public CheckerBuilder(NCheck.ICheckerFactory factory) : base(factory)
+        {
+        }
+    }
+}
