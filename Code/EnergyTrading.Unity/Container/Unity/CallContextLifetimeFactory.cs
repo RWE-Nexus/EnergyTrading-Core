62cdd75574602293032a6456c660ca7a037dd665
Paul Hatcher <paul.hatcher@rwe.com>
2014-07-09 17:34:36 +0100

    Improve documentation

--- Code/EnergyTrading.Unity/Container/Unity/CallContextLifetimeFactory.cs
+++ Code/EnergyTrading.Unity/Container/Unity/CallContextLifetimeFactory.cs
@@ -4,8 +4,15 @@
 
     using Microsoft.Practices.Unity;
 
+    /// <summary>
+    /// Factory for creating call context lifetime managers.
+    /// </summary>
     public static class CallContextLifetimeFactory
     {
+        /// <summary>
+        /// Gets a new <see cref="LifetimeManager"/>
+        /// </summary>
+        /// <returns>A new <see cref="WebCallContextLifetimeManager"/> if we are running in a web application, otherwise a new <see cref="CallContextLifetimeManager"/></returns>
         public static LifetimeManager Manager()
         {
             if (HttpContext.Current != null)
--- Code/EnergyTrading.Unity/Container/Unity/CallContextLifetimeManager.cs
+++ Code/EnergyTrading.Unity/Container/Unity/CallContextLifetimeManager.cs
@@ -5,20 +5,29 @@
 
     using Microsoft.Practices.Unity;
 
+    /// <summary>
+    /// Contextual lifetime manager.
+    /// <para>
+    /// Uses <see cref="CallContext" /> to store values.
+    /// </para>
+    /// </summary>
     public class CallContextLifetimeManager : LifetimeManager
     {
-        private readonly string key = string.Format("CallContextLifeTimeManager_{0}", Guid.NewGuid()); 
+        private readonly string key = string.Format("CallContextLifeTimeManager_{0}", Guid.NewGuid());
 
+        /// <copydocfrom cref="LifetimeManager.GetValue" />
         public override object GetValue()
         {
             return CallContext.GetData(key);
         }
 
+        /// <copydocfrom cref="LifetimeManager.GetValue" />
         public override void SetValue(object newValue)
         {
             CallContext.SetData(key, newValue);
         }
 
+        /// <copydocfrom cref="LifetimeManager.GetValue" />
         public override void RemoveValue()
         {
             CallContext.FreeNamedDataSlot(key); 
