f880b3d3711e6b4f6fbb513fe28cf6594da11bf6
Simon Hanlon <simon.hanlon@rwe.com>
2014-07-17 12:13:39 +0100

    Added CheckOrRetrieveValue extension for a MappingContext to provide a standard pattern for simple caching of values based on name

---
+++ Code/EnergyTrading.Core/Mapping/Extensions/ContextExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace EnergyTrading.Mapping.Extensions
+{
+    using System;
+
+    public static class ContextExtensions
+    {
+        /// <summary>
+        /// Returns the value stored in the Context if it exists 
+        /// Otherwise attempts to retrieve the value, add it to the Context and then return it
+        /// If neither is possible returns default(T)
+        /// </summary>
+        /// <typeparam name="T">the type of object expected</typeparam>
+        /// <param name="context">The Context to check</param>
+        /// <param name="name">The name to check for the value within the Context</param>
+        /// <param name="retriever">Function used to retrieve the object if it is not already in the context</param>
+        /// <returns>the value in the context or the value returned from retriever or default(T) if retriever is null</returns>
+        public static T CheckOrRetrieveValue<T>(this Context context, string name, Func<T> retriever = null)
+        {
+            if (context == null || string.IsNullOrWhiteSpace(name))
+            {
+                return default(T);
+            }
+
+            if (context.Exists(name))
+            {
+                return context.Value<T>(name);
+            }
+
+            if (retriever == null)
+            {
+                return default(T);
+            }
+
+            context.Set(name, retriever());
+            return context.Value<T>(name);
+        }
+    }
+}
---
+++ Code/EnergyTrading.UnitTest/Mapping/Extensions/ContextExtensionsFixture.cs
@@ -0,0 +1,57 @@
+﻿namespace EnergyTrading.UnitTest.Mapping.Extensions
+{
+    using EnergyTrading.Mapping;
+    using EnergyTrading.Mapping.Extensions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ContextExtensionsFixture
+    {
+        [Test]
+        public void CheckOrRetrieveValueWithNullContext()
+        {
+            var candidate = ContextExtensions.CheckOrRetrieveValue(null, "name", () => 3);
+            Assert.That(candidate, Is.EqualTo(default(int)));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("     ")]
+        public void CheckOrRetrieveValueWithInvalidNames(string name)
+        {
+            var candidate = new Context().CheckOrRetrieveValue(name, () => 3);
+            Assert.That(candidate, Is.EqualTo(default(int)));
+        }
+
+        [Test]
+        public void CheckOrRetrieveValueRetrieverIsNull()
+        {
+            var candidate = new Context().CheckOrRetrieveValue<int>("name");
+            Assert.That(candidate, Is.EqualTo(default(int)));
+        }
+
+        [Test]
+        public void ValueFromContextIsReturnedIfPresent()
+        {
+            var count = 0;
+            var context = new Context();
+            context.Set("name", 55);
+            var candidate = context.CheckOrRetrieveValue("name", () => ++count);
+            Assert.That(candidate, Is.EqualTo(55));
+            Assert.That(count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ValueFromRetrieverIsReturnedAndStoredInContext()
+        {
+            var count = 0;
+            var context = new Context();
+            var candidate = context.CheckOrRetrieveValue("name", () => ++count);
+            Assert.That(candidate, Is.EqualTo(count));
+            Assert.That(count, Is.EqualTo(1));
+            Assert.That(context.Value<int>("name"), Is.EqualTo(1));
+        }
+    }
+}
