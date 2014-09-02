be12be97439291abe55f190509cc569dfb4e2963
Simon Hanlon <simon.hanlon@rwe.com>
2014-09-02 12:56:59 +0100

    Move search cache key generation functions to EnergyTrading.Core so that they can be used by the client

---
+++ Code/EnergyTrading.Core/Search/SearchExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace EnergyTrading.Search
+{
+    using System;
+    using System.Text;
+
+    using EnergyTrading.Xml.Serialization;
+
+    public static class SearchExtensions
+    {
+        public static Contracts.Search.Search ToSearch<TContract>(this string key)
+        {
+            var searchString = FromBase64(key.Replace("-", "/").Replace(")", "+").Replace("(", "=")).Substring(typeof(TContract).Name.Length);
+            return searchString.DeserializeDataContractXmlString<Contracts.Search.Search>();
+        }
+
+        private static string FromBase64(string source)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(source));
+        }
+
+        private static string ToBase64(string source)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(source));
+        }
+
+        public static string ToKey<TContract>(this Contracts.Search.Search search)
+        {
+            var searchString = typeof(TContract).Name + search.DataContractSerialize();
+            var baseString = ToBase64(searchString);
+            return baseString.Replace("/", "-").Replace("+", ")").Replace("=", "(");
+        }
+    }
+}
---
+++ Code/EnergyTrading.UnitTest/Search/SearchExtensionsFixture.cs
@@ -0,0 +1,55 @@
+﻿namespace EnergyTrading.UnitTest.Search
+{
+    using System.Web;
+
+    using EnergyTrading.Contracts.Search;
+    using EnergyTrading.Search;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SearchExtensionsFixture
+    {
+        [Test]
+        public void KeysForSameSearchAreTheSame()
+        {
+            var search = SearchBuilder.CreateSearch();
+            search.AddSearchCriteria(SearchCombinator.And)
+                    .AddCriteria("TargetSourceSystem.Id", SearchCondition.Equals, "1", true)
+                    .AddCriteria("SourceSystemType", SearchCondition.Equals, "SourceSystem", false);
+            var key1 = search.ToKey<int>();
+            var key2 = search.ToKey<int>();
+            Assert.AreEqual(key1, key2);
+        }
+
+        [Test]
+        public void SearchKeysAreReversible()
+        {
+            var search = SearchBuilder.CreateSearch();
+            search.AddSearchCriteria(SearchCombinator.And)
+                    .AddCriteria("TargetSourceSystem.Id", SearchCondition.Equals, "1", true)
+                    .AddCriteria("SourceSystemType", SearchCondition.Equals, "SourceSystem", false);
+            var key1 = search.ToKey<int>();
+            var candidate = key1.ToSearch<int>();
+            Assert.AreEqual(candidate.SearchFields.Combinator, SearchCombinator.And);
+            Assert.AreEqual(candidate.SearchFields.Criterias[0].Criteria[0].Field, "TargetSourceSystem.Id");
+            Assert.AreEqual(candidate.SearchFields.Criterias[0].Criteria[1].Field, "SourceSystemType");
+            Assert.AreEqual(candidate.SearchFields.Criterias[0].Criteria[0].Condition, SearchCondition.Equals);
+            Assert.AreEqual(candidate.SearchFields.Criterias[0].Criteria[1].Condition, SearchCondition.Equals);
+            Assert.AreEqual(candidate.SearchFields.Criterias[0].Criteria[0].ComparisonValue, "1");
+            Assert.AreEqual(candidate.SearchFields.Criterias[0].Criteria[1].ComparisonValue, "SourceSystem");
+        }
+
+        [Test]
+        public void KeyIsNotAffectedByUrlEncoding()
+        {
+            var search = SearchBuilder.CreateSearch();
+            search.AddSearchCriteria(SearchCombinator.And)
+                    .AddCriteria("TargetSourceSystem.Id", SearchCondition.Equals, "1", true)
+                    .AddCriteria("SourceSystemType", SearchCondition.Equals, "SourceSystem", false);
+            var key1 = search.ToKey<int>();
+            var urlEncodedKey = HttpUtility.UrlEncode(key1);
+            Assert.AreEqual(key1, urlEncodedKey);
+        }
+    }
+}
